dd5cd6868ae5d024a4fe63f20541babc128ed1d9
LiuKai <3513864951@qq.com>
2024-09-18 15:51:05 +0800

    :sparkles: feat: 实现题库功能开发(CRUD)，添加部分接口权限校验
    - 实现了题库和题库题目的CRUD操作，包括创建、编辑、删除和查询功能。
    - 通过开发新的REST API和扩展MyBatis映射支持题库管理。
    - 集成了权限检查注解以确保数据操作的安全性。

---
+++ src/main/java/com/voyager/interview/service/QuestionService.java
@@ -0,0 +1,61 @@
+package com.voyager.interview.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.voyager.interview.common.BaseResponse;
+import com.voyager.interview.model.dto.question.QuestionQueryRequest;
+import com.voyager.interview.model.entity.Question;
+import com.voyager.interview.model.vo.QuestionVO;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 问题服务
+ *
+ *
+ */
+public interface QuestionService extends IService<Question> {
+
+    /**
+     * 校验数据
+     *
+     * @param question
+     * @param add 对创建的数据进行校验
+     */
+    void validQuestion(Question question, boolean add);
+
+    /**
+     * 获取查询条件
+     *
+     * @param questionQueryRequest
+     * @return
+     */
+    QueryWrapper<Question> getQueryWrapper(QuestionQueryRequest questionQueryRequest);
+    
+    /**
+     * 获取问题封装
+     *
+     * @param question
+     * @param request
+     * @return
+     */
+    QuestionVO getQuestionVO(Question question, HttpServletRequest request);
+
+    /**
+     * 分页获取问题封装
+     *
+     * @param questionPage
+     * @param request
+     * @return
+     */
+    Page<QuestionVO> getQuestionVOPage(Page<Question> questionPage, HttpServletRequest request);
+
+    /**
+     * 分页获取问题列表（仅管理员可用）
+     *
+     * @param questionQueryRequest
+     * @return
+     */
+    public Page<Question> listQuestionByPage(QuestionQueryRequest questionQueryRequest);
+}
